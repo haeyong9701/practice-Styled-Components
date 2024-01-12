d1b306ab4dce54348aa92e3548e851700e679459
haeyong9701 <haeyong9701@gmail.com>
2024-01-12 23:16:59 +0900

    feat: Nesting 문법

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,43 @@
 import styled from "styled-components";
+import facebook from "./facebook.svg";
 
-const Button = styled.button`
+const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+  background-color: blue;
+`;
+
+const StyledButton = styled.button`
   background-color: #6750a4;
   border: none;
   color: #ffffff;
   padding: 16px;
+
+  ${Icon} {
+    margin-right: 4px;
+    background-color: red;
+  }
+
+  &:hover,
+  &:active {
+    background-color: #463770;
+
+    ${Icon} {
+      opacity: 0.2;
+    }
+  }
 `;
+
+const Button = ({ children }) => {
+  return (
+    <>
+      <StyledButton>
+        <Icon src={facebook} alt="facebook icon" />
+        {children}
+      </StyledButton>
+      <Icon src={facebook} alt="facebook icon" />
+    </>
+  );
+};
 
 export default Button;
