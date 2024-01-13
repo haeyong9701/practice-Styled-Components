d2b23ab13bcafee32b6efdd2816df697fc835fb5
haeyong9701 <haeyong9701@gmail.com>
2024-01-13 22:26:09 +0900

    feat: 스타일 재사용: css 함수

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,14 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const SIZES = {
   large: 24,
   medium: 20,
   small: 16,
 };
+
+const fontSize = css`
+  font-size: ${({ font }) => SIZES[font] ?? SIZES["medium"]}px;
+`;
 
 const Icon = styled.img`
   width: 16px;
@@ -17,7 +21,7 @@ const StyledButton = styled.button`
   border: none;
   color: #ffffff;
   padding: 16px;
-  font-size: ${({ font }) => SIZES[font] ?? SIZES["medium"]}px;
+  ${fontSize}
 
   ${Icon} {
     margin-right: 4px;
